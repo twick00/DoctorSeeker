9ef0098495f8ee55b6bc2b1ea3d759dffb4e82a7
Tyler Wickline <twick00@gmail.com>
2018-03-23 15:36:04 -0700

    Adds some styling for cards and corrected path to doctor info.

--- src/js/docSearch.js
+++ src/js/docSearch.js
@@ -4,7 +4,7 @@ let api = process.env.exports.apiKey;
 export function searchDoctor(pageLimit = 10, docName = "", location = "wa-seattle", symptom = "") {
   return new Promise(function(resolve, reject) {
     const request = new XMLHttpRequest();
-    let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}query=${symptom}&location=${location}&limit=${pageLimit}&user_key=${api}fields=profile`;
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&query=${symptom}&location=${location}&limit=${pageLimit}&user_key=${api}`;
 
     request.onload = function() {
       if(this.statusText === 'OK') {
@@ -13,9 +13,29 @@ export function searchDoctor(pageLimit = 10, docName = "", location = "wa-seattl
         reject(console.error(this.statusText));
       }
     };
+    request.open("GET", url, true);
+    request.send();
   });
 }
+export function getPhone(data) {
 
+}
+export function getSite(data) {
+
+}
+export function getBoolNewPatients(data) {
+  let acceptingPatientsObj = [];
+  for(let i = 0; i < data.data.practices.length; i++)
+  {
+    if (data.data.practices[i].accepts_new_patients === true) 
+    {
+      acceptingPatientsObj[i] = {name:data.data.practices.name, accepting:true};
+    }
+    else {
+      acceptingPatientsObj[i] = {name:data.data.practices.name, accepting:false};
+    }
+  }
+}
 
 //https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=${location}&limit=${pageLimit}&user_key=${api}
 
--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import "./../css/styles.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { searchDoctor } from "./docSearch";
+import _ from 'lodash';
 
 let errorTemplate = function(string){
   return `Error: ${string}`;
@@ -11,35 +12,38 @@ $(document).ready(function() {
   $("#searchField").submit(function(event) {
     event.preventDefault();
     $("#results").empty();
-    if($("#searchField").val() == "")
-    {
-      $("results").append(errorTemplate("Please enter a value."));
-      return;
-    }
+    let result;
     let data = searchDoctor($("#page-limit").val(), $("#searchName").val(),$("location").val(),$("#searchSymptom").val());
-    let parsedData = cardTemplate(data);
-    parsedData.forEach(function(card) {
-      $("#results").append(card);
+    data.then(function(response) {
+      let parsedData = cardTemplate(response);
+      parsedData.forEach(function(card) {
+        $("#results").append(card);
+      });
     });
+    // let parsedData = cardTemplate(data);
+    // parsedData.forEach(function(card) {
+    //   $("#results").append(card);
+    // });
   });
 });
 
-let cardTemplate = function(data)
+function cardTemplate(data)
 {
   data = JSON.parse(data);
-  const array = [];
-  data.forEach(function(profile) {
-    let shortBio = profile.bio.split(" ").slice(0, 25).join(" ").concat("...");
+  let array = [];
+  for(let i = 0; i < _.size(data.data); i++)
+  {
+    let shortBio = data.data[i].profile.bio.split(" ").slice(0, 25).join(" ").concat("...");
     let card = `
     <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="${profile.image_url}" alt="${profile.first_name} ${profile.last_name}">
+      <img class="card-img-top" src="${data.data[i].profile.image_url}" alt="${data.data[i].profile.first_name} ${data.data[i].profile.last_name}">
       <div class="card-body">
-        <h5 class="card-title">${profile.first_name} ${profile.last_name}</h5>
+        <h5 class="card-title">${data.data[i].profile.first_name} ${data.data[i].profile.last_name}</h5>
         <p class="card-text">${shortBio}</p>
       </div>
     </div>
     `;
     array.push(card);
-  });
+  }
   return array;
-};
+}
